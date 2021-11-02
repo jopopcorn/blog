6e3bdc9d04846fb0f3677988ff6fb2bb086f43b7
Sooyeon Jo <supop1009@gmail.com>
2021-11-02 21:50:38 +0900

    feat : User data class 수정 및 생성자 추가

--- app/src/main/java/com/example/blog/data/User.kt
+++ app/src/main/java/com/example/blog/data/User.kt
@@ -1,8 +1,10 @@
 package com.example.blog.data
 
 data class User(
-    val id : String,
-    var nickname : String,
-    var introduce : String,
-    var blog : Blog
-)
+    val id: Int,
+    var nickname: String,
+    var profileImageUrl: String,
+    var blog: Blog
+) {
+    constructor(id: Int) : this(id = id, "user$id", "", Blog(id))
+}
