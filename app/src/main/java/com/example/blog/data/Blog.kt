77454c926fbc3e6053fde179ffebec6520961290
Sooyeon Jo <supop1009@gmail.com>
2021-11-02 21:49:50 +0900

    feat : Blog data class 수정 및 생성자 추가

--- app/src/main/java/com/example/blog/data/Blog.kt
+++ app/src/main/java/com/example/blog/data/Blog.kt
@@ -1,10 +1,21 @@
 package com.example.blog.data
 
 data class Blog(
+    val id: Int,
     var name: String,
+    var introduction: String,
     var url: String,
     var numberOfNeighbor: Int,
     var backgroundImageUrl: String,
-    var profileImageUrl: String,
     var postList: ArrayList<Post>
-)
+) {
+    constructor(id: Int) : this(
+        id = id,
+        name = "user${id}의 블로그",
+        introduction = "",
+        url = "user${id}.blockblog.com",
+        numberOfNeighbor = 0,
+        backgroundImageUrl = "",
+        postList = arrayListOf()
+    )
+}
