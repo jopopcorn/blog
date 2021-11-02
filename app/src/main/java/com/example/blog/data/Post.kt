1c4f76485264752c4a2c563584d932a984ab07f4
Sooyeon Jo <supop1009@gmail.com>
2021-11-02 21:50:19 +0900

    feat : Post data class 수정 및 생성자 추가

--- app/src/main/java/com/example/blog/data/Post.kt
+++ app/src/main/java/com/example/blog/data/Post.kt
@@ -9,4 +9,15 @@ data class Post(
     var numberOfComment: Int,
     val date: String,
     var commentList: ArrayList<Comment>
-)
+) {
+    constructor(id: Int, title: String, content: String, date: String) : this(
+        id = id,
+        title = title,
+        content = content,
+        image = arrayListOf(),
+        0,
+        0,
+        date = date,
+        commentList = arrayListOf()
+    )
+}
